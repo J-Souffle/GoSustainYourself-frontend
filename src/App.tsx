0e2dd8b3682bb464d56c3f822f1e7938c5fa417d
Joshua Cancio <145736028+J-Souffle@users.noreply.github.com>
2025-03-29 20:54:45 -0400

    Working shadcn dashboard page

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardHeader, CardContent } from "@/components/ui/card";
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
+import DashboardPage from './dashboard/page'
+
+function App() {
+  return (
+    <DashboardPage />
+  );
+}
+
+export default App;
